3052857ead2425bd251c0f3fc4ef7111d92cc6d9
User -2023ZWXER\Administrator <winsam003@gmail.com>
2024-03-13 17:01:45 +0900

    css 첫 커밋

---
+++ sports-complex/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>** 여기는 Header 입니다! **</h1>
+            <h3>안녕하세요! 최주먹밥입니다(*￣3￣)╭</h3>
+        </header>
+    );
+};
