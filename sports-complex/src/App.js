f2fc46da805d7eff134195b0868033a9cacf37ca
gjeongjjj <qwertyw@naver.com>
2024-03-14 18:05:57 +0900

    로그인 페이지 하는중

--- sports-complex/src/App.js
+++ sports-complex/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MiddleMenu from './components/MiddleMenu';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Header />
+      <Login />
       <MiddleMenu />
       <Footer />
     </div>
