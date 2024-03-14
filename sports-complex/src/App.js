39608935ce30517f8a91d5539dab4022996fd22e
Seonghyun Baek <bsh6954@gmail.com>
2024-03-14 18:06:10 +0900

    pagebanner start

--- sports-complex/src/App.js
+++ sports-complex/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Banner from './components/PageBanner';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
 
