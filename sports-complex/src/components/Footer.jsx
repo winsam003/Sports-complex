60d61119eb53b4e3356559d879dfcba051ff8cfc
Seonghyun Baek <bsh6954@gmail.com>
2024-03-14 11:17:36 +0900

    main css pull

--- sports-complex/src/components/Footer.jsx
+++ sports-complex/src/components/Footer.jsx
@@ -3,27 +3,26 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-            <div>
-                <ul className="footer_ul">
-                    <ol className="footer_ol">
-                        <li className="footer_li">개인정보처리방침</li>
-                        <li className="footer_li">영상정보처리운영방침</li>
+            <div className='footer_list'>
+                <ul className='footer_ul'>
+                    <ol className='footer_ol'>
+                        <li className='footer_li'>개인정보처리방침</li>
+                        <li className='footer_li'>영상정보처리운영방침</li>
                     </ol>
-                    <ol className="footer_ol">
-                        <li className="footer_li">이메일수집거부</li>
-                        <li className="footer_li">찾아오시는길</li>
+                    <ol className='footer_ol'>
+                        <li className='footer_li'>이메일수집거부</li>
+                        <li className='footer_li'>찾아오시는길</li>
                     </ol>
                 </ul>
             </div>
 
             <div className='company_info'>
-                <img src='img/Arcana3.gif' />
+                <img src='img/Arcana3.gif' className='footer_logo' />
             </div>
 
             <div className='consulting_telNum'>
 
             </div>
-
 
         </footer>
     );
