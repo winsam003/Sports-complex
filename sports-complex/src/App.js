acd8c59f836deb960f2c133df1cee68274c80ab2
gjeongjjj <qwertyw@naver.com>
2024-03-15 15:33:11 +0900

    비밀번호 찾기(+인증 방법 두개)+폰트

--- sports-complex/src/App.js
+++ sports-complex/src/App.js
@@ -3,11 +3,15 @@ import Header from './components/Header';
 import Banner from './components/PageBanner';
 import Footer from './components/Footer';
 import MiddleMenu from './components/MiddleMenu';
+import Login from './components/Login';
+import FindPassword from './components/FindPassword';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <FindPassword />
       <Footer />
     </div>
 
