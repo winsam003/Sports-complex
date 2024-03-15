8616eff5b272566ae5e0d9a14f38c83a404e3028
Seonghyun Baek <bsh6954@gmail.com>
2024-03-15 16:05:13 +0900

    app

--- sports-complex/src/App.js
+++ sports-complex/src/App.js
@@ -3,13 +3,11 @@ import Header from './components/Header';
 import Banner from './components/PageBanner';
 import Footer from './components/Footer';
 import MiddleMenu from './components/MiddleMenu';
-import FacilityInformation from "./components/FacilityInformation";
 
 function App() {
   return (
     <div>
       <Header />
-      <FacilityInformation />
       <Footer />
     </div>
 
