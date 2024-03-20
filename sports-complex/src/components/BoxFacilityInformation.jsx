232678814a76b8088d3319cd00b12a915c3fbe78
User -2023ZWXER\Administrator <winsam003@gmail.com>
2024-03-20 16:36:24 +0900

    임시커밋

--- sports-complex/src/components/BoxFacilityInformation.jsx
+++ sports-complex/src/components/BoxFacilityInformation.jsx
@@ -4,18 +4,18 @@ export default function BoxFacilityInformation() {
     return (
         <div className='BoxFacilityInformation_div'>
             <div className='BoxFacilityInformation_img_div'>
-                <img src='' className='BoxFacilityInformation_img'></img>
+                <img src='img/시설안내.jpg' className='BoxFacilityInformation_img'></img>
             </div>
             <p>시설명</p>
-            <p>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
-            <p className='BoxFacilityInformation_top_p'>규격</p>
-            <p className='BoxFacilityInformation_top_p'></p>
-            <p className='BoxFacilityInformation_top_p'>바닥재질</p>
-            <p className='BoxFacilityInformation_top_p'></p>
-            <p className='BoxFacilityInformation_top_p'>관람석</p>
-            <p className='BoxFacilityInformation_top_p'></p>
+            <p>FitNest 종합운동장</p>
+            <p className='BoxFacilityInformation_top_p'>건축면적</p>
+            <p className='BoxFacilityInformation_top_p'>29,220㎡</p>
+            <p className='BoxFacilityInformation_top_p'>연면적</p>
+            <p className='BoxFacilityInformation_top_p'>91,720㎡</p>
+            <p className='BoxFacilityInformation_top_p'>주차장</p>
+            <p className='BoxFacilityInformation_top_p'>300면</p>
             <p className='BoxFacilityInformation_top_p'>준공연도</p>
-            <p className='BoxFacilityInformation_top_p'></p>
+            <p className='BoxFacilityInformation_top_p'>2024년</p>
         </div>
     );
 };
