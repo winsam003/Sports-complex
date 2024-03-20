c117d330847f635bf242eb19e77b014c88f59038
gjeongjjj <qwertyw@naver.com>
2024-03-20 18:17:01 +0900

    본인인증 페이지 + 헤더크기 (+ 이상한 로그인창 주의.)

--- sports-complex/src/components/PhoneAthentication.jsx
+++ sports-complex/src/components/PhoneAthentication.jsx
@@ -3,7 +3,7 @@ import "./PhoneAthentication.css"
 export default function PhoneAthentication() {
     return (
         <div className="PhoneAthentication_box">
-            <img src="img/dora.jpg" alt="" style={{width: "200px", height: "200px"}} />
+            <img src="img/phoneMark.jpg" alt="" style={{width: "200px", height: "200px"}} />
             <div className="PhoneAthentication_title">핸드폰 인증</div>
             <button>인증하기</button>
             <p className="PhoneAthentication_call">안내 : 1588-2486</p>
