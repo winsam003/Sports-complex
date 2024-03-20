96708e03e7be81d82c4f45fac0d0d2ef38bb7cea
Seonghyun <bsh6954@gmail.com>
2024-03-20 21:36:43 +0900

    이메일수집거부, 찾아오시는길

--- sports-complex/src/components/Pages/InfoHandlingPolicyPage.jsx
+++ sports-complex/src/components/Pages/InfoHandlingPolicyPage.jsx
@@ -3,7 +3,7 @@ import PageBanner from '../PageBanner';
 import Footer from '../Footer';
 
 // 개인정보처리방침
-export default function InfoHandlingPolicy() {
+export default function InfoHandlingPolicyPage() {
     return (
         <div>
             <Header />
--- sports-complex/src/components/Pages/VideoHandlePage.jsx
+++ sports-complex/src/components/Pages/VideoHandlePage.jsx
@@ -2,8 +2,8 @@ import Header from '../Header';
 import PageBanner from '../PageBanner';
 import Footer from '../Footer';
 
-// 찾아오시는길
-export default function VisitWay() {
+// 영상정보처리기운영방침
+export default function VideoHandlePage() {
     return (
         <div>
             <Header />
