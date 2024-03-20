702f0987e986cafa951d80bb5422a03c216f8f94
gjeongjjj <qwertyw@naver.com>
2024-03-20 16:34:03 +0900

    수업 일정표

--- sports-complex/src/components/ClassScheduleDetail.jsx
+++ sports-complex/src/components/ClassScheduleDetail.jsx
@@ -5,13 +5,13 @@ export default function ClassScheduleDetail(){
         <div className="ClassScheduleDetail_Box">
             <span>수영</span>
             <span>접영</span>
-            <span>2024-03-19</span>
-            <span>2024-03-31</span>
+            <span>2024.03.19</span>
+            <span>2024.03.31</span>
             <span>성인</span>
             <span>50</span>
             <span>12/50</span>
             <span>0</span>
-            <span>80000</span>
+            <span>80,000</span>
             <button>신청</button>
         </div>
     )
