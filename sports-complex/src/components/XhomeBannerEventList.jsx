432436a9b81511103f64249ae463841577ca4734
gjeongjjj <qwertyw@naver.com>
2024-03-21 18:12:19 +0900

    홈 이벤트 관리 시스템

---
+++ sports-complex/src/components/XhomeBannerEventList.jsx
@@ -0,0 +1,20 @@
+import './XhomeBannerEventList.css';
+
+export default function XhomeBannerEventList() {
+
+    const data = [
+        {
+            "eventCode" : "00001",
+            "eventName" : "김수한무두루미 생일잔치",
+            "cpCode" : "1",
+            "eventUpload" : "24.03.19",
+            "eventUploadfile" : "moon.jpg",
+        }
+    ]
+
+    return(
+        <div className="XhomeBannerEventList_div">
+            
+        </div>
+    )
+}
