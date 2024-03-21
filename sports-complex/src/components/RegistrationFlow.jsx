264a7f2aa929b9e1a95bf72e96879f15bbf70242
Seonghyun Baek <bsh6954@gmail.com>
2024-03-21 10:29:12 +0900

    회원가입 완료

--- sports-complex/src/components/RegistrationFlow.jsx
+++ sports-complex/src/components/RegistrationFlow.jsx
@@ -15,22 +15,22 @@ export default function RegistrationFlow() {
                     <span className={`${currentPage === 'JoinPage1' ? 'textBold' : ""}`}>Step 1</span>
                     <span className={`${currentPage === 'JoinPage1' ? 'textBold' : ""}`}>약관동의</span>
                 </li>
-                <span>&raquo;</span>
+                {/* <span>&raquo;</span> */}
                 <li>
                     <span className={`${currentPage === 'JoinPage2' ? 'textBold' : ""}`}>Step 2</span>
                     <span className={`${currentPage === 'JoinPage2' ? 'textBold' : ""}`}>회원유형</span>
                 </li>
-                <span>&raquo;</span>
+                {/* <span>&raquo;</span> */}
                 <li>
                     <span className={`${currentPage === 'JoinPage3' ? 'textBold' : ""}`}>Step 3</span>
                     <span className={`${currentPage === 'JoinPage3' ? 'textBold' : ""}`}>본인확인</span>
                 </li>
-                <span>&raquo;</span>
+                {/* <span>&raquo;</span> */}
                 <li>
                     <span className={`${currentPage === 'JoinPage4' ? 'textBold' : ""}`}>Step 4</span>
                     <span className={`${currentPage === 'JoinPage4' ? 'textBold' : ""}`}>개인정보입력</span>
                 </li>
-                <span>&raquo;</span>
+                {/* <span>&raquo;</span> */}
                 <li>
                     <span>Step 5</span>
                     <span>가입 완료</span>
---
+++ sports-complex/src/components/VisitWay.jsx
@@ -0,0 +1,9 @@
+import './VisitWay.css'
+
+// 찾아오시는길
+export default function VisitWay() {
+    return (
+        <div>
+        </div>
+    )
+}
