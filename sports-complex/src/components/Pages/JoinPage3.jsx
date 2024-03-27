97d8b8f6051f2fa2ad2ce6f3946a265fa8651ba7
User -2023ZWXER\Administrator <winsam003@gmail.com>
2024-03-27 18:12:13 +0900

    회원가입 / check박스

--- sports-complex/src/components/Pages/JoinPage3.jsx
+++ sports-complex/src/components/Pages/JoinPage3.jsx
@@ -1,12 +1,10 @@
 import PageBanner from '../PageBanner';
 import FindPassword from '../FindPassword';
-import UserTypeAgree from '../UserTypeAgree';
 import RegistrationFlow from './../RegistrationFlow';
 import { useLocation } from 'react-router';
 
 export default function JoinPage3() {
     
-    const nextPage = window.location.pathname.replace("/", "");
     const location = useLocation();
     const { memberType } = location.state;
 
