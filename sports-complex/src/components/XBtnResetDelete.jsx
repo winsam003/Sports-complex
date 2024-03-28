7fdd13c2db0125ad411db9a89f1292197a9bed79
gjeongjjj <qwertyw@naver.com>
2024-03-28 11:42:29 +0900

    두번클릭으로 reset 현상

--- sports-complex/src/components/XBtnResetDelete.jsx
+++ sports-complex/src/components/XBtnResetDelete.jsx
@@ -3,12 +3,12 @@ import './XBtnResetDelete.css'
 import axios from 'axios'
 
 // 초기화 삭제 버튼
-export default function XResetDeleteBtn({del}) {
+export default function XResetDeleteBtn({del, handleReset}) {
 
     return (
         <div>
             <div className='XResetDeleteBtn'>
-                <button>초기화</button>
+                <button onClick={handleReset}>초기화</button>
                 <button onClick={del}>삭제</button>
             </div>
         </div>
