d6bf06bd68d1a32b283a4915e2a159ff87a5d984
gjeongjjj <qwertyw@naver.com>
2024-03-29 20:32:51 +0900

    시설 등록

--- sports-complex/src/components/XReantalPlaceNewone.jsx
+++ sports-complex/src/components/XReantalPlaceNewone.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from 'react'
 import './XReantalPlaceNewone.css'
 import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 export default function XReantalPlaceNewone() {
 
     // =================== 카테고리 합쳐서 코드 만들기 =============================
+    // 인서트
+
+    const [formPlace, setFormPlace] = useState({
+        spacecode: '', 
+        spacename: '', 
+        spaceprice: '', 
+        parkspace: '' 
+    })
+    console.log(formPlace);
+
+
+
     const [placeType, setPlaceType] = useState("");
     const [spacelist, setSpaceList] = useState([]);
+    // 최종 코드 
+    const [fullCode, setFullCode] = useState("");
 
     // 시설 코드 만들기
     const placeTypeCode = (e) =>{
@@ -36,9 +51,15 @@ export default function XReantalPlaceNewone() {
         // 전체 코드 만들어주기. (기본코드 + 시설 타입 + 번호 + 코트/층)
         const fullCode = facilityCode + placeType + (count +1) + facilityLast;
         console.log("fullCode="+fullCode);
+        // 담아주기
+        setFullCode(fullCode);
+        setFormPlace({
+            ...formPlace, 
+            spacecode: fullCode
+        })
         
     }, [spacelist]);
-
+    
     //===============================================================================================
 
     // 이름, 가격 Hook
@@ -48,37 +69,66 @@ export default function XReantalPlaceNewone() {
 
     const makePlaceName = (e) => {
         setPlaceName(e);
+        setFormPlace({
+            ...formPlace, 
+            spacename: e
+        })
     }
     // console.log(placeName);
     
     const makePlacePrice = (e) => {
         setPlacePrice(e);
+        setFormPlace({
+            ...formPlace, 
+            spaceprice: e
+        })
     }
     // console.log(placePrice);
     
     const makePlaceCount = (e) => {
         setPlaceCount(e);
+        setFormPlace({
+            ...formPlace, 
+            parkspace: e == '' ? 1 : e
+        })
+        
     }
     // console.log(placeCount);
+
     
+    //===============================================================================================
+
 
     // 등록 버튼. onClick
+    const navigate = useNavigate(); 
+    // 이거 대체 왜 밖에다가 선언해야되나요
     const spaceInsert = () => {
-        console.log(!placeType);
-        if(placeType){
-            if (placeName) {
-                if(placePrice) {
+        console.log(formPlace);
 
-                    
-                } else{
-                    alert('대관 시설 가격을 입력해주세요. ');
-                }
-            } else{
-                alert('대관 시설명을 입력해주세요. ');
-            }
-        } else {
+        if(!placeType){
             alert('대관 시설 종류를 선택해주세요. ');
+            return;
         }
+        if(!placeName){
+            alert('대관 시설명을 입력해주세요. ');
+            return;
+        }
+        if(!placePrice){
+            alert('대관 시설 가격을 입력해주세요. ');
+            return;
+        }
+
+
+        axios.post('/space/spaceInsert', formPlace
+        ).then((response) => {
+            alert(response.data);
+            navigate('/XRentalPlaceControllPage');
+            
+        } ).catch((error) => {
+            console.error("spaceInsert error : ", error);
+        })
+
+
     }
 
 
@@ -97,19 +147,19 @@ export default function XReantalPlaceNewone() {
                 </select>
                 
                 <p>시설 이름</p>
-                <input type="text" name='placeName' id='placeName'  
+                <input type="text" name='spacename' id='spacename'  
                                                 placeholder='시설 이름을 입력하세요.'
                                                 value={placeName}
                                                 onChange={(e) => makePlaceName(e.target.value)} ></input>
 
                 <p>시설 가격</p>
-                <input type="text" name='placePrice' id='placePrice' 
+                <input type="text" name='spaceprice' id='placePrice' 
                                                 placeholder='숫자를 입력하시오.'
                                                 value={placePrice}
                                                 onChange={(e) => makePlacePrice(e.target.value)} ></input>
                 <p>시설 자리 수</p>
                 <input type="text" name='placeCount' id='placeCount' 
-                                                placeholder='숫자를 입력하시오.'
+                                                placeholder='입력하지 않으시면 자동 한 자리로 등록됩니다.'
                                                 value={placeCount}
                                                 onChange={(e) => makePlaceCount(e.target.value)} ></input>
 
