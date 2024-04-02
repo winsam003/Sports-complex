38dd52f3d2bd06d22989faaadb5aee3affda4748
User -2023ZWXER\Administrator <winsam003@gmail.com>
2024-04-02 14:33:35 +0900

    에러 해결중

--- sports-complex/src/index.js
+++ sports-complex/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />
