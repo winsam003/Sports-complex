64bb3707addc10401fd2e987dca0a2ec78fbf2bf
gjeongjjj <qwertyw@naver.com>
2024-04-08 18:21:57 +0900

    이벤트 첨부파일을 올리고 싶지만 실패.

--- sports-complex-back/src/main/java/com/example/demo/controller/EventController.java
+++ sports-complex-back/src/main/java/com/example/demo/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.Event;
 import com.example.demo.service.EventServiceImpl;
@@ -82,11 +84,84 @@ public class EventController {
 	@PostMapping(value = "/eventinsert", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> eInsert(@RequestBody Event dto){
 		
-		if(service.EventInsert(dto) > 0) {
-			return ResponseEntity.status(HttpStatus.OK).body("이벤트 등록 완료. 목록으로 이동");
-		} else {
-			return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("이벤트 등록 실패");
+		try {
+			MultipartFile file = dto.getEventfilef();
+			
+			String originalFilename = null;
+
+			
+			if(file != null && !file.isEmpty()) {
+				originalFilename = file.getOriginalFilename();
+				log.info("첨부파일 : " + originalFilename);
+			}
+			
+			Event event = Event.builder()
+					.eventname(dto.getEventname())
+					.eventdetail(dto.getEventdetail())
+					.eventfacility(dto.getEventfacility())
+					.eventtime(dto.getEventtime())
+					.eventfor(dto.getEventfor())
+					.eventtype(dto.getEventtype())
+//					.eventuploadfile(file.getOriginalFilename())
+					.eventuploadfile(originalFilename)
+					.stfid(dto.getStfid())
+					.eventfilef(null)
+					.build();
+					
+			
+			String realPath = "C:\\jgj\\TeamSSJ\\Sports-complex\\sports-complex-back\\src\\main\\webapp\\images\\eventBoard\\";
+			
+			
+			// 솔직히 무슨 기준으로 배포 전, 후 를 나눠야할지 모르겠음 일단 같은 폴더로 지정했음
+			if(realPath.contains(".TeamSSJ."))
+				realPath = "C:\\jgj\\TeamSSJ\\Sports-complex\\sports-complex-back\\src\\main\\webapp\\images\\eventBoard\\";
+			else 
+				realPath = "C:\\jgj\\TeamSSJ\\Sports-complex\\sports-complex-back\\src\\main\\webapp\\images\\eventBoard\\";
+			
+			
+			// 1.1. 해당 위치에 폴더가 존재하지 않다면 만들기
+			File file1 = new File(realPath);
+			if(!file1.exists()) {
+				file1.mkdir();
+			}
+			
+			// 1.2. 저장 할 파일 데이터가 존재한다면 저장 경로에 파일 이름을 붙여주고 파일 복사 (저장)
+			MultipartFile uploadfilef = event.getEventfilef();
+			if(uploadfilef != null && !uploadfilef.isEmpty()) {
+				String f2 = realPath + uploadfilef.getOriginalFilename();
+				File f1 = new File(f2);
+				uploadfilef.transferTo(f1);
+			}
+			
+			
+			
+			log.info(dto);
+			
+			if(service.EventInsert(dto) > 0) {
+				return ResponseEntity.status(HttpStatus.OK).body("이벤트 등록 완료. 목록으로 이동");
+			} else {
+				return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("이벤트 등록 실패");
+			}
+			
+			
+		} catch (Exception e) {
+			log.error("이벤트 게시판 등록 중 오류 발생 : ", e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이벤트 등록 중 오류가 발생했습니다.");
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	} // eInsert
 	
