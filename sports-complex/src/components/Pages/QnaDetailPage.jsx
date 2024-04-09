fac5e8178ebdffe12bcc96e9f90e8fce76bc67c1
Seonghyun Baek <bsh6954@gmail.com>
2024-04-09 11:53:20 +0900

    파일다운로드 렌더링 해결

--- sports-complex/src/components/Pages/QnaDetailPage.jsx
+++ sports-complex/src/components/Pages/QnaDetailPage.jsx
@@ -4,13 +4,10 @@ import { useLocation } from 'react-router-dom';
 
 // 문의게시판 상세 페이지
 export default function QnaDetailPage() {
-    const location = useLocation();
-    const qnaData = location.state ? location.state.qnaData : null;
-
     return (
         <div>
             <PageBanner />
-            <QnaDetail qnaData={qnaData} />
+            <QnaDetail />
         </div>
     )
 }
