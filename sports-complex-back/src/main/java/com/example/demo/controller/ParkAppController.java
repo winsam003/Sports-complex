fe4d37d74edfed498cb0f9afff3f1f0f7c4c5b5f
gjeongjjj <qwertyw@naver.com>
2024-04-22 09:47:24 +0900

    오늘 주차 해야지

--- sports-complex-back/src/main/java/com/example/demo/controller/ParkAppController.java
+++ sports-complex-back/src/main/java/com/example/demo/controller/ParkAppController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
