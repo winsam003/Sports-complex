3ece578d2ab483e0defdf091ff9591cde96207dc
gjeongjjj <qwertyw@naver.com>
2024-04-29 16:44:04 +0900

    parkstate??

--- sports-complex/src/components/ParkingRequestContent.jsx
+++ sports-complex/src/components/ParkingRequestContent.jsx
@@ -126,7 +126,7 @@ export default function ParkingRequestContent({getUserName, getUserID}) {
 
     // 등록 
     const parkAppSubmit = async () => {
-        console.log(myparklist[0].parkstate);
+        // console.log(myparklist[0].parkstate);
         if(!spacecodeApp){
             alert('주차장을 선택해주세요.');
             return;
