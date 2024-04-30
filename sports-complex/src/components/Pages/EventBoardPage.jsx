a9717aa3267d83b21d2b10e2b8115a375fc87366
gjeongjjj <qwertyw@naver.com>
2024-04-30 18:16:29 +0900

    미들메뉴 수정

--- sports-complex/src/components/Pages/EventBoardPage.jsx
+++ sports-complex/src/components/Pages/EventBoardPage.jsx
@@ -1,7 +1,13 @@
 import PageBanner from '../PageBanner';
 import EventBoard from '../EventBoard';
+import { useEffect } from 'react';
 
 export default function EventBoardPage() {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div>
             <PageBanner />
--- sports-complex/src/components/Pages/EventDetailPage.jsx
+++ sports-complex/src/components/Pages/EventDetailPage.jsx
@@ -2,8 +2,13 @@ import PageBanner from '../PageBanner';
 import EventBoardDetail from '../EventBoardDetail';
 import { useLocation } from 'react-router';
 import XEventBoardDetail from '../XEventBoardDetail';
+import { useEffect } from 'react';
 
 export default function EventDetailPage(){
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
